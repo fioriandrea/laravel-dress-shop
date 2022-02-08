b4578b9cd7581bf224d1f0ab9209d9712bf29f4c
fioriandrea <andrea.fiori.1998@gmail.com>
2022-02-08 19:53:41 +0100

    move global variables to controller base class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,20 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\View;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $categories;
+    protected $sizes;
+
+    public function __construct() 
+    {
+        $this->categories = ['Shirts', 'Shoes', 'Suits', 'Hats', 'Pants', 'Ties'];
+        $this->sizes = ['S', 'M', 'L', 'XL'];
+        View::share('categories', $this->categories);
+        View::share('sizes', $this->sizes);
+    }
 }
