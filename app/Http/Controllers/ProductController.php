36ed5b729c4f0d17b52b85833117e9718b554a1e
fioriandrea <andrea.fiori.1998@gmail.com>
2022-02-09 11:51:27 +0100

    redirect back when deleting a product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -58,7 +58,7 @@ class ProductController extends Controller
     {
         $product = DataLayer::getProduct($id);
         DataLayer::unlistProduct($id);
-        return redirect()->route('product_list')->with('success', 'Product ' . $product->name . ' deleted succesfully');
+        return redirect()->back()->with('success', 'Product ' . $product->name . ' deleted succesfully');
     }
 
     public function postEditProduct(Request $request, $id)
